68157524925ac0f66f994db416c444fd30982c3b
OloinMilsom <keporagebora@gmail.com>
2016-10-07 10:23:28 +0100

    initial

---
+++ BasicSFMLProject/main.cpp
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////// 
+// Headers 
+//////////////////////////////////////////////////////////// 
+#include "stdafx.h" 
+#ifdef _DEBUG 
+#pragma comment(lib,"sfml-graphics-d.lib") 
+#pragma comment(lib,"sfml-audio-d.lib") 
+#pragma comment(lib,"sfml-system-d.lib") 
+#pragma comment(lib,"sfml-window-d.lib") 
+#pragma comment(lib,"sfml-network-d.lib") 
+#else 
+#pragma comment(lib,"sfml-graphics.lib") 
+#pragma comment(lib,"sfml-audio.lib") 
+#pragma comment(lib,"sfml-system.lib") 
+#pragma comment(lib,"sfml-window.lib") 
+#pragma comment(lib,"sfml-network.lib") 
+#endif 
+#pragma comment(lib,"opengl32.lib") 
+#pragma comment(lib,"glu32.lib") 
+
+int main(int argc, char *argv[]) {
+	// Create the main window 
+	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "SFML First Program");
+
+
+
+	system("PAUSE");
+}
